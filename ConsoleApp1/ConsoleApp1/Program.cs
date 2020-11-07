8f670132a9ee25ba36d4402c34a7142d4daeaa83
ITAcademy <DemoITAcademy@outlook.com>
2020-11-07 14:56:16 +0530

    DemoITAcademy Changes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World project from VS 2019 !");
+            Console.WriteLine("Hello World project from VS 2019 ITAcademy!");
+            Console.WriteLine("Hello World project from VS 2019 Demo ITAcademy!");
         }
     }
 }
